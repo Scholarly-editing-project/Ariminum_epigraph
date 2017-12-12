5ec270cf064a870f86ed73bd07a2e8bbf206e053
gspinaci <spino9330@gmail.com>
2017-12-12 18:52:58 +0100

    Added useful comments and a new file containing constants

---
+++ constants.js
@@ -0,0 +1,18 @@
+/**
+ * This file contains only a JSON object which contains useful constants
+ */
+
+// Init the JSON object
+const CONSTANTS = {
+  PORT: 8080,
+  NAME: 'Ariminum Epigraph',
+  PAGES: {
+    INDEX_PAGE: 'index.html',
+  },
+  FOLDERS: {
+    ASSETS_FOLDER: 'assets',
+  },
+}
+
+// Export the object
+module.exports = CONSTANTS
--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+/**
+ * Express server used to manage requests 
+ */
 
+// Import all needed libraries
 const express = require('express')
 const app = express()
 
+// Store some useful and global constants
 const DIRNAME = __dirname
-const PORT = 8080
 
+// Import the JSON structure stored inside the file "constants.js"
+const CONSTANTS = require('./constants')
+
+/**
+ * This function manage the root request
+ * 
+ * eg. localhost:8080 from a browser will trigger this event 
+ * and it will run its content
+ * 
+ * This will return the index page
+ */
 app.get('/', function (req, res) {
-  res.sendFile(`${DIRNAME}/index.html`)
+  res.sendFile(`${DIRNAME}/${CONSTANTS.PAGES.INDEX_PAGE}`)
 })
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!')
+/**
+ * This function allows front-end html files to load any asset file 
+ * stored inside the folder named "assets"
+ */
+app.use(express.static(CONSTANTS.FOLDERS.ASSETS_FOLDER))
+
+/**
+ * This method open the port designed PORT
+ * 
+ * Now the server is still listening on this port for requests
+ */
+app.listen(CONSTANTS.PORT, function () {
+  console.log(`${CONSTANTS.NAME} listening on port ${CONSTANTS.PORT}!`)
 })
