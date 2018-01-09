807aef018f12ea36f3a809d3b19636d174e349ee
gspinaci <spino9330@gmail.com>
2018-01-09 17:53:48 +0100

    Fix epigraphs

---
+++ db/schemas/people.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+let scheme = mongoose.Schema({
+  _id: String,
+  name: {
+    praenomen: String,
+    nomen: String,
+    cognomen: String
+  },
+  gender: String,
+  age: String,
+  social_status: String,
+  city: String,
+  career: [{
+    role: String,
+    grade: String,
+		place: String,
+    type: String,
+		description: String
+  }],
+  relation: [{
+    type: String,
+    person: String
+  }],
+  actions: [{
+    role: String,
+    name: String
+  }],
+  documents: [String]
+})
+
+module.exports = {
+  scheme
+}
